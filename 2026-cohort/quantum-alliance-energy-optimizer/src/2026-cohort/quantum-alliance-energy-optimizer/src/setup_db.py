c6856dfe091c6c0d737ca6affb436ed9814346f0
seyi-omisore <sharlerx1@gmail.com>
2026-08-27 22:19:51 -0400

    Delete 2026-cohort/quantum-alliance-energy-optimizer/src/2026-cohort/quantum-alliance-energy-optimizer/src/setup_db.py

--- 2026-cohort/quantum-alliance-energy-optimizer/src/2026-cohort/quantum-alliance-energy-optimizer/src/setup_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('energy_optimizer.db')
-conn.executescript(open('schema.sql').read())
-conn.commit()
-
-cur = conn.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print([r[0] for r in cur.fetchall()])
